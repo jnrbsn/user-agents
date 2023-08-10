350f4050e6d422d7447abbc5a37a0e7cb23aac26
Anvesh <anvesh_mora@outlook.com>
2023-08-10 20:55:27 +0530

    update python script

--- update.py
+++ update.py
@@ -1,15 +1,9 @@
-import json
-import os
-import random
-import re
-import time
+import json, os, random, re, time, requests
 from itertools import product
-
-import requests
 from github import Github
 from lxml import html
 
-user_agents_file_name = 'user-agents.json'
+user_agents_file_name = 'user_agents.json'
 user_agents_file_path = os.path.join(
     os.path.dirname(__file__), user_agents_file_name)
 
@@ -29,37 +23,29 @@ _os_field_exclude_patterns = [
 
 _saved_user_agents = None
 
-
 def get_saved_user_agents():
     global _saved_user_agents
-
     if _saved_user_agents is None:
         with open(user_agents_file_path, 'r') as f:
             _saved_user_agents = json.load(f)
-
     return _saved_user_agents
-
 
 def get_latest_user_agents():
     user_agents = []
     base_url = 'https://www.whatismybrowser.com/guides/the-latest-user-agent/'
-
     for browser in ('chrome', 'firefox', 'safari', 'edge'):
         time.sleep(1)
         response = requests.get(
             ''.join((base_url, browser)),
             headers={'User-Agent': random.choice(get_saved_user_agents())},
         )
-
         elems = html.fromstring(response.text).cssselect('td li span.code')
-
         browser_uas = []
         for elem in elems:
             ua = elem.text_content().strip()
             if not ua.startswith('Mozilla/5.0 ('):
                 continue
             browser_uas.append(ua)
-
         for ua in browser_uas:
             os_type = ua[len('Mozilla/5.0 ('):ua.find(')')].lower()
             os_fields = [p.strip() for p in os_type.split(';')]
@@ -67,31 +53,26 @@ def get_latest_user_agents():
             if any(p.match(f) for p, f in product(
                     _os_field_exclude_patterns, os_fields)):
                 continue
-
             if any(p.match(f) for p, f in product(
                     _os_field_include_patterns, os_fields)):
                 user_agents.append(ua)
-
     return user_agents
-
 
 def json_dump(obj):
     return json.dumps(obj, indent=4).strip() + '\n'
 
-
 def update_files_on_github(new_user_agents_json):
     gh = Github(os.environ['GITHUB_TOKEN'])
     repo = gh.get_repo(os.environ['GITHUB_REPOSITORY'])
-    for branch in ('main', 'gh-pages'):
-        f = repo.get_contents(user_agents_file_name, ref=branch)
-        repo.update_file(
-            f.path,
-            message=f'Update {user_agents_file_name} on {branch} branch',
-            content=new_user_agents_json,
-            sha=f.sha,
-            branch=branch,
-        )
-
+    branch = 'main'
+    f = repo.get_contents(user_agents_file_name, ref=branch)
+    repo.update_file(
+        f.path,
+        message=f'Update {user_agents_file_name} on {branch} branch',
+        content=new_user_agents_json,
+        sha=f.sha,
+        branch=branch,
+    )
 
 if __name__ == '__main__':
     old_user_agents = get_saved_user_agents()
