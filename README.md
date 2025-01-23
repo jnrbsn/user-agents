d19487af30c1a537435344417131a99cc7b427ee
Jonathan Robson <jnrbsn@gmail.com>
2025-01-23 14:15:45 -0600

    Major rewrite to improve correctness and reliability

--- README.md
+++ README.md
@@ -3,3 +3,63 @@
 Latest user agent strings for major browsers and OSs; checks for updates daily:
 
 <https://jnrbsn.github.io/user-agents/user-agents.json>
+
+## Update 2025-01-23
+
+**TL;DR:** I made it better, more correct, and more reliable! :tada:
+
+When I created this repo ~4.5 years ago, it originally screen-scraped the user
+agent strings from [whatismybrowser.com][1]. Around two years ago, that website
+blocked GitHub Actions. So then I thought I had a clever idea of using
+[The Internet Archive][2] and also making it take regular snapshots. But then,
+because legal problems, The Internet Archive was temporarily shutdown for a
+month last year. Regardless of the shutdown, the GitHub Action just fails
+sometimes for various reasons. It has become fragile and complex, and I don't
+like it.
+
+Meanwhile, in the years since I created this repo, the state of the browser
+user agent string has changed in significant ways. Driven by frustration with
+constant user-agent-sniffing bugs, as well as a desire to reduce browser
+fingerprinting, every major browser now hard-codes most of its user agent
+string. There are now fixed strings for macOS, Windows, and Linux, and things
+like CPU architectures and engine versions are now fixed as well.
+
+* **Chrome:** In Chromium, the user agent string is now [well-documented][3].
+  On the desktop version, the only parts that aren't fixed are the OS/platform
+  (which can only be one of a handful of fixed strings) and the major version
+  of Chromium (the minor parts of the version are hard-coded as zeros).
+* **Firefox:** The [macOS version][4] and [Windows version][5] are now fixed in
+  Firefox, and the [CPU architecture][6] is now hard-coded for all OSs. On
+  Linux, the OS part of the user agent is usually just generic anyway, with a
+  hard-coded [special case for Ubuntu][7]. Also, the [Gecko version][8] has
+  been fixed for many years on desktop.
+* **Safari:** In WebKit, the [macOS version][9] and the [WebKit version][10]
+  are now fixed. The only thing that Safari adds to the WebKit user agent is
+  the Safari version at the end.
+* **Edge:** Edge is based on Chromium and mostly uses the same user agent
+  string, but it adds the [major version of Edge at the end][11], which
+  somewhat amusingly, is the same as the major version of Chromium.
+
+So now, instead of using fragile methods of screen-scraping a website of
+dubious quality, I get the latest major version of each of the above browsers
+from official sources and then generate the following 16 user agent strings:
+
+* two most-recent versions of Chrome on macOS, Windows, and Linux (Chrome rolls
+  out releases very gradually, so it's common to have many users on the
+  previous version) (6 strings)
+* latest and ESR (extended support release) versions of Firefox on macOS,
+  Windows, generic Linux, and Ubuntu (8 strings)
+* latest version of Safari on macOS
+* latest version of Edge on Windows
+
+[1]: https://www.whatismybrowser.com/guides/the-latest-user-agent/
+[2]: https://web.archive.org/
+[3]: https://www.chromium.org/updates/ua-reduction/
+[4]: https://hg.mozilla.org/mozilla-central/rev/ba73a7622f6366fa1c1446fa96d42b93166d91e0
+[5]: https://hg.mozilla.org/mozilla-central/rev/fbbdeef57f8252daf5400305c289119c73382942
+[6]: https://bugzilla.mozilla.org/show_bug.cgi?id=1559747
+[7]: https://hg.mozilla.org/mozilla-central/rev/2d4419b5c1dfb8d6ae096cfb5c01dc75f33dffe8
+[8]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent/Firefox
+[9]: https://github.com/WebKit/WebKit/commit/49a2b0400d3f6a16e8d604e112da4b38d22cbd02
+[10]: https://github.com/WebKit/WebKit/commit/1a54b3453655e45af52850475f50fc01de2fe710
+[11]: https://learn.microsoft.com/en-us/microsoft-edge/web-platform/user-agent-guidance#user-agent-strings
--- update.py
+++ update.py
@@ -2,107 +2,50 @@ import json
 import os
 import random
 import re
-import time
-from datetime import datetime, timedelta, timezone
-from itertools import count, product
+import sys
+from functools import wraps
 
 import requests
 from github import Github
-from lxml import html
 
 
 user_agents_file_name = 'user-agents.json'
 user_agents_file_path = os.path.join(
     os.path.dirname(__file__), user_agents_file_name)
 
-_os_field_include_patterns = [
-    re.compile(r'^windows nt \d+\.\d+$', flags=re.IGNORECASE),
-    re.compile(r'^macintosh$', flags=re.IGNORECASE),
-    re.compile(r'^linux (x86_64|i686)$', flags=re.IGNORECASE),
-]
-_os_field_exclude_patterns = [
-    re.compile(r'\bwindows mobile\b', flags=re.IGNORECASE),
-    re.compile(r'\bxbox\b', flags=re.IGNORECASE),
-    re.compile(r'\biphone\b', flags=re.IGNORECASE),
-    re.compile(r'\bipad\b', flags=re.IGNORECASE),
-    re.compile(r'\bipod\b', flags=re.IGNORECASE),
-    re.compile(r'\bandroid\b', flags=re.IGNORECASE),
-]
-
 _saved_user_agents = None
 
 
-class wayback_machine:
+def json_dump(obj):
+    return json.dumps(obj, indent=2).strip() + '\n'
 
-    @classmethod
-    def timestamped_url(cls, timestamp, url):
-        return f'https://web.archive.org/web/{timestamp}/{url}'
 
-    @classmethod
-    def save_url(cls, url):
-        print(f'Saving URL on web.archive.org: {url}')
-        response = requests.post(
-            f'https://web.archive.org/save/{url}',
-            data={'url': url, 'capture_all': 'on'},
-        )
-        response.raise_for_status()
-
-    @classmethod
-    def get_latest_timestamp(cls, url):
-        print(f'Getting latest archived snapshot for URL: {url}')
-        try:
-            response = requests.get(
-                'https://archive.org/wayback/available',
-                params={'url': url},
-            )
-            response.raise_for_status()
-            response_data = response.json()
-            return response_data['archived_snapshots']['closest']['timestamp']
-        except (requests.HTTPError, KeyError):
-            response = requests.get(
-                'https://web.archive.org/cdx/search/cdx',
-                params={
-                    'url': url,
-                    'output': 'json',
-                    'limit': '-1',
-                    'fastLatest': 'true',
-                    'fl': 'timestamp',
-                },
-            )
-            response.raise_for_status()
-            response_data = response.json()
-            return response_data[1][0]
-
-    @classmethod
-    def get_auto_updated_url(cls, url, max_age_days=7):
-        timestamp = cls.get_latest_timestamp(url)
-
-        ts_dt = datetime.strptime(
-            timestamp, '%Y%m%d%H%M%S').replace(tzinfo=timezone.utc)
-        utc_now = datetime.now(tz=timezone.utc)
-        age_days = (utc_now - ts_dt).days
-
-        if age_days >= max_age_days:
-            cls.save_url(url)
-            for _ in range(6):
-                time.sleep(10)
-                new_timestamp = cls.get_latest_timestamp(url)
-                if new_timestamp > timestamp:
-                    print(
-                        f'Updated timestamp for URL from {timestamp} to '
-                        f'{new_timestamp}: {url}')
-                    timestamp = new_timestamp
-                    break
+def with_cli_status(msg):
+    def decorator(func):
+        @wraps(func)
+        def wrapper():
+            sys.stdout.write(f'{msg}... ')
+            sys.stdout.flush()
+            try:
+                result = func()
+            except Exception:
+                sys.stdout.write('FAILED\n\n')
+                sys.stdout.flush()
+                raise
             else:
-                if age_days >= max_age_days * 2:
-                    raise RuntimeError(
-                        f'Timestamp for URL is {age_days} days old: {url}')
-                else:
-                    print(
-                        'WARNING: Took longer than 60 seconds to save URL; '
-                        'skipping for now')
+                sys.stdout.write('OK\n\n')
+                sys.stdout.flush()
+                return result
+        return wrapper
+    return decorator
 
-        return cls.timestamped_url(timestamp, url)
+
+def requests_get(url, params=None):
+    params = params or {}
+    user_agents = get_saved_user_agents()
+    response = requests.get(url, params=params, headers={'User-Agent': random.choice(user_agents)})
+    response.raise_for_status()
+    return response
 
 
 def get_saved_user_agents():
@@ -115,52 +58,135 @@ def get_saved_user_agents():
     return _saved_user_agents
 
 
+@with_cli_status('Getting Chrome user agents')
+def generate_chrome_user_agents():
+    user_agents = []
+    platform_channels = (
+        ('Mac', ('Stable', 'Extended'), 'Macintosh; Intel Mac OS X 10_15_7'),
+        ('Windows', ('Stable', 'Extended'), 'Windows NT 10.0; Win64; x64'),
+        ('Linux', ('Stable',), 'X11; Linux x86_64'),
+    )
+    for platform, channels, ua_platform in platform_channels:
+        versions = set()
+        for channel in channels:
+            response = requests_get(
+                'https://chromiumdash.appspot.com/fetch_releases',
+                params={
+                    'channel': channel,
+                    'platform': platform,
+                    'num': 10,
+                    'offset': 0,
+                })
+            data = response.json()
+            versions.update(int(x['version'].split('.', 1)[0]) for x in data)
+        versions = sorted(versions)
+        for version in versions[-2:]:
+            user_agents.append(
+                (f'Mozilla/5.0 ({ua_platform}) AppleWebKit/537.36 (KHTML, like Gecko) '
+                    f'Chrome/{version}.0.0.0 Safari/537.36'))
+    return user_agents
+
+
+@with_cli_status('Getting Firefox user agents')
+def generate_firefox_user_agents():
+    user_agents = []
+    ua_platforms = (
+        'Macintosh; Intel Mac OS X 10.15',
+        'Windows NT 10.0; Win64; x64',
+        'X11; Linux x86_64',
+        'X11; Ubuntu; Linux x86_64',
+    )
+    trains = ('esr', 'release')
+    versions = set()
+    for train in trains:
+        response = requests_get(
+            'https://whattrainisitnow.com/api/release/schedule/',
+            params={'version': train})
+        data = response.json()
+        versions.add(int(data['version'].split('.', 1)[0]))
+    versions = sorted(versions)
+    for ua_platform in ua_platforms:
+        for version in versions:
+            user_agents.append(
+                f'Mozilla/5.0 ({ua_platform}; rv:{version}.0) Gecko/20100101 Firefox/{version}.0')
+    return user_agents
+
+
+@with_cli_status('Getting Safari user agents')
+def generate_safari_user_agents():
+    # XXX: these are not public APIs so I'm trying two different sources
+    # to reduce the chance of breakage
+    user_agents = []
+    sources = (
+        (
+            'https://developer.apple.com/tutorials/data/index/safari-release-notes',
+            lambda data: [
+                x['title'] for x in data['interfaceLanguages']['swift'][0]['children']
+                if x['type'] == 'article'],
+        ),
+        (
+            'https://developer.apple.com/tutorials/data/documentation/safari-release-notes.json',
+            lambda data: [
+                x['title'] for x in data['references'].values()
+                if x['kind'] == 'article'],
+        ),
+    )
+    exc = None
+    for url, title_getter in sources:
+        try:
+            response = requests_get(url)
+            data = response.json()
+            versions = set()
+            for title in title_getter(data):
+                if re.search(r'(?i)\bbeta\b', title):
+                    continue
+                match = re.search(r'[0-9]+(\.[0-9]+)*', title)
+                versions.add(match[0])
+            versions = sorted(versions, key=lambda v: tuple(int(x) for x in v.split('.')))
+            version = versions[-1]
+            break
+        except Exception as e:
+            exc = e
+    else:
+        raise RuntimeError('failed to get latest version of Safari') from exc
+    user_agents.append(
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) '
+        f'Version/{version} Safari/605.1.15')
+    return user_agents
+
+
+@with_cli_status('Getting Edge user agents')
+def generate_edge_user_agents():
+    user_agents = []
+    response = requests_get(
+        'https://raw.githubusercontent.com/MicrosoftDocs/Edge-Enterprise/refs/heads/public'
+        '/edgeenterprise/microsoft-edge-relnote-stable-channel.md')
+    versions = set()
+    for line in response.text.splitlines():
+        match = re.match(
+            r'(?i)^#{2,} +version +([0-9]+)(\.[0-9]+)+ *: *[a-z]+ +[0-9]{1,2} *, *[0-9]{4}', line)
+        if match is None:
+            continue
+        versions.add(int(match[1]))
+    versions = sorted(versions)
+    version = versions[-1]
+    user_agents.append(
+        (f'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+            f'Chrome/{version}.0.0.0 Safari/537.36 Edg/{version}.0.0.0'))
+    return user_agents
+
+
 def get_latest_user_agents():
     user_agents = []
-    base_url = 'https://www.whatismybrowser.com/guides/the-latest-user-agent/'
-
-    for browser in ('chrome', 'firefox', 'safari', 'edge'):
-        time.sleep(1)
-        url = wayback_machine.get_auto_updated_url(base_url + browser)
-        print(f'Getting user agents for {browser}')
-        response = requests.get(
-            url,
-            headers={'User-Agent': random.choice(get_saved_user_agents())},
-        )
-        if response.status_code >= 400:
-            print(response.text)
-            response.raise_for_status()
-
-        elems = html.fromstring(response.text).cssselect('td li span.code')
-
-        browser_uas = []
-        for elem in elems:
-            ua = elem.text_content().strip()
-            if not ua.startswith('Mozilla/5.0 ('):
-                continue
-            browser_uas.append(ua)
-
-        for ua in browser_uas:
-            os_type = ua[len('Mozilla/5.0 ('):ua.find(')')].lower()
-            os_fields = [p.strip() for p in os_type.split(';')]
-
-            if any(p.match(f) for p, f in product(
-                    _os_field_exclude_patterns, os_fields)):
-                continue
-
-            if any(p.match(f) for p, f in product(
-                    _os_field_include_patterns, os_fields)):
-                user_agents.append(ua)
-
-    return sorted(set(user_agents))
+    user_agents.extend(generate_chrome_user_agents())
+    user_agents.extend(generate_firefox_user_agents())
+    user_agents.extend(generate_safari_user_agents())
+    user_agents.extend(generate_edge_user_agents())
+    return user_agents
 
 
-def json_dump(obj):
-    return json.dumps(obj, indent=4).strip() + '\n'
-
-
+@with_cli_status('Updating files on GitHub')
 def update_files_on_github(new_user_agents_json):
-    print('Updating files on GitHub')
     gh = Github(os.environ['GITHUB_TOKEN'])
     repo = gh.get_repo(os.environ['GITHUB_REPOSITORY'])
     for branch in ('main', 'gh-pages'):
@@ -178,12 +204,12 @@ if __name__ == '__main__':
     old_user_agents = get_saved_user_agents()
     old_user_agents_json = json_dump(old_user_agents)
     print(f'old_user_agents = {old_user_agents_json}')
-    assert len(old_user_agents) >= 4
+    assert len(old_user_agents) >= 7
 
     new_user_agents = get_latest_user_agents()
     new_user_agents_json = json_dump(new_user_agents)
     print(f'new_user_agents = {new_user_agents_json}')
-    assert len(new_user_agents) >= 4
+    assert len(new_user_agents) >= 7
 
     if old_user_agents_json != new_user_agents_json:
         update_files_on_github(new_user_agents_json)
